35a51386abaf8198972a21526534f34919fc24a4
chandima1313 <chandima.colombage@inovaitsys.com>
2020-02-24 13:37:23 +0530

    Chapter3

---
+++ JavaProject/src/Chapter3/CharDemo.java
@@ -0,0 +1,19 @@
+package Chapter3;
+
+/**
+ * C03 L03
+ * 
+ * Demonstrate char data type.
+ */
+public class CharDemo {
+
+    public static void main(String[] args) {
+        char ch1, ch2;
+
+        ch1 = 88;
+        ch2 = 'Y';
+
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
