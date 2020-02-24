1337944380b99a940d253ffc338816f7bead0968
chandima1313 <chandima.colombage@inovaitsys.com>
2020-02-24 11:56:21 +0530

    Chapter2

---
+++ JavaProject/src/Chapter2/Example.java
@@ -0,0 +1,13 @@
+package Chapter2;
+
+/**
+ * C02 L01
+ * 
+ * This is a simple Java program. Call this file "Example.java"
+ */
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("This is a simple Java program.");
+    }
+}
